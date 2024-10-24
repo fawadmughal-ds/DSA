10ea675156ceb5d743631fe65a8bf86185cb36c7
Fawad Mughal <fawadmughal0333@gmail.com>
2024-10-24 22:30:22 +0500

    Complete Stack is Emplemented with Node.

--- Stack_with_Node.py
+++ Stack_with_Node.py
@@ -2,3 +2,48 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
+
+class Stack:
+    def __init__(self):
+        self.stack=None
+    
+    def is_Empty(self):
+        if self.stack is None:
+            return True
+
+    def push(self, val):
+        data=Node(val)
+        data.next=self.stack
+        self.stack = data
+
+    def pop(self):
+        if self.is_Empty():
+            raise IndexError("List is Empty")
+        else:
+            data=self.stack.data
+            self.stack = self.stack.next
+            return data
+        
+    def peek(self):
+        if self.is_Empty():
+            raise IndexError("List is Empty")
+        return self.stack.data
+    
+    def length(self):
+        num=0
+        while self.stack is not None:
+            num+=1
+            self.stack = self.stack.next
+        return num 
+    
+
+
+s=Stack()
+s.push(9)
+s.push(19)
+s.push(89)
+s.push(10)
+print(s.peek())
+s.pop()
+print(s.peek())
+print(s.length())
