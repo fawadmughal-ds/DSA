372825797f214f09eed9de869079973113b0b83f
Fawad Mughal <fawadmughal0333@gmail.com>
2024-10-25 10:36:03 +0500

    completed queue with list .

--- Queue_with_list.py
+++ Queue_with_list.py
@@ -6,3 +6,25 @@ class Queue:
 
     def is_Empty(self):
         return len(self.queue) ==0
+    
+    def Enqueue(self,val):
+        self.queue.append(val)
+
+    def Dequeue(self):
+        if self.is_Empty() is None:
+            raise IndexError("List is Empty")
+        self.queue.pop(0)
+
+    def get_front(self):
+        if self.is_Empty() is None:
+            raise IndexError("List is Empty")
+        return self.queue[0]
+    
+    def get_rear(self):
+        if self .is_Empty() is None:
+            raise IndexError("List is Empty")
+        return self.queue[-1]
+    
+    def length(self):
+        return len(self.queue)
+    
