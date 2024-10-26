9685c6c4f6c316a6c6e1ea2c6c9b3123e2e1119a
Fawad Mughal <fawadmughal0333@gmail.com>
2024-10-26 23:14:54 +0500

    Full Queue is implemented with Node Class

--- Queue_with_Node.py
+++ Queue_with_Node.py
@@ -20,7 +20,18 @@ class Queue:
             self.rear.next=data
         self.rear = data
         self.item_count +=1
-        def get_front(self):
+
+    def Dequeue(self):
+        if self.is_Empty():
+            raise IndexError("Queue is Empty")
+        elif self.rear == self.front:
+            self.front = None
+            self.rear = None
+        else:
+            self.front =self.front.next
+        self.item_count -=1
+
+    def get_front(self):
         if self.is_Empty():
             raise IndexError("List is Empty")
         return self.front.data
@@ -33,3 +44,14 @@ class Queue:
     def length(self):
         return self.item_count
     
+
+q=Queue()
+q.Enqueue(9)
+q.Enqueue(19)
+q.Enqueue(89)
+q.Enqueue(10)
+print(q.get_front())
+q.Dequeue()
+print(q.get_rear())
+print(q.get_front())
+print(q.length())
