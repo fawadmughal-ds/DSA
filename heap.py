e925ad66b9372aebef7bfb3bac850f0395ca6edb
Fawad Mughal <fawadmughal0333@gmail.com>
2024-11-08 23:05:03 +0500

    Heap Implemetned:

---
+++ heap.py
@@ -0,0 +1,37 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, key):
+        self.heap.append(key)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_max(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_up(self, index):
+        parent_index = (index - 1) // 2
+        if index > 0 and self.heap[index] > self.heap[parent_index]:
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+            self._heapify_up(parent_index)
+
+    def _heapify_down(self, index):
+        largest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+        if largest != index:
+            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self._heapify_down(largest)
+
